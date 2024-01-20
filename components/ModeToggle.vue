cd0520f878f3429a635718b309fe2bf297d394be
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-01-20 17:02:35 -0300

    Add ModeToggle component

---
+++ components/ModeToggle.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const toggleState = ref(false)
+const checkboxOne = ref(false)
+const checkboxTwo = ref(false)
+const person = ref('pedro')
+
+function handleClick() {
+  alert('hello!')
+}
+</script>
+
+<template>
+  <DropdownMenuRoot v-model:open="toggleState">
+    <DropdownMenuTrigger aria-label="Customise options">
+      <Button>test</Button>
+    </DropdownMenuTrigger>
+
+
+  </DropdownMenuRoot>
+</template>
+
+<style></style>
