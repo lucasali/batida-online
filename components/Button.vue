0bd7f84a411c90802dee1e853da475942417b430
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-01-20 17:02:33 -0300

    Add Button component

---
+++ components/Button.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+export interface ButtonProps {
+  type?: 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link'
+  size?: 'default' | 'sm' | 'lg' | 'icon'
+  disabled?: boolean
+  href?: string
+  onClick?: (event: MouseEvent) => void
+}
+
+const { type = 'default', size = 'default', disabled, href, onClick } = defineProps<ButtonProps>()
+
+const classes = computed(() => {
+  return [
+    'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+    {
+      'h-10 px-4 py-2': size === 'default',
+      'h-9 rounded-md px-3': size === 'sm',
+      'h-11 rounded-md px-8': size === 'lg',
+      'h-10 w-10': size === 'icon',
+      'bg-primary text-primary-foreground hover:bg-primary/90': type === 'default',
+      'bg-destructive text-destructive-foreground hover:bg-destructive/90': type === 'destructive',
+      'border border-input bg-background hover:bg-accent hover:text-accent-foreground': type === 'outline',
+      'bg-secondary text-secondary-foreground hover:bg-secondary/80': type === 'secondary',
+      'hover:bg-accent hover:text-accent-foreground': type === 'ghost',
+      'text-primary underline-offset-4 hover:underline': type === 'link'
+    }
+  ]
+})
+</script>
+
+<template>
+  <button :class="classes">
+    <slot></slot>
+  </button>
+</template>
+
+<style></style>
