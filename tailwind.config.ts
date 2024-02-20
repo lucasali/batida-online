0d9c881aebf59c6bd6b28ada819d56a98841ba40
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-19 22:59:19 -0300

    Remove unnecessary content from tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,6 @@ import AnimationPlugin from 'tailwindcss-animate'
 
 export default {
   darkMode: ['class'],
-  content: [
-    './components/**/*.{js,vue,ts}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './app.vue',
-    './error.vue'
-  ],
   theme: {
     container: {
       center: true,
@@ -81,4 +73,4 @@ export default {
     }
   },
   plugins: [AnimationPlugin]
-} satisfies Config
+} satisfies Partial<Config>
