123653d69bbabf4be49cfde41f091ed85f482cc1
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-21 23:27:17 -0300

    Update nuxt.config.ts with new modules and configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['radix-vue/nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode', 'radix-vue/nuxt', 'nuxt-icon'],
-  css: ['~/assets/css/tailwind.css', '~/assets/css/fonts.css'],
-})
+  css: ["~/assets/css/tailwind.css", "~/assets/css/fonts.css"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "radix-vue/nuxt", "nuxt-icon", "@nuxt/image"],
+  colorMode: {
+    classSuffix: "",
+  },
+  tailwindcss: {
+    editorSupport: true,
+  },
+});
