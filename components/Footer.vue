c2049edac67b7c193bc1a8e5ce190082e58ce5cc
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-10-20 11:12:55 -0300

    feat: Create Footer and Header

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { format } from '@formkit/tempo'
+
+const date = format(new Date(), 'long')
+</script>
+
+<template>
+  <footer>
+    <p class="text-pretty text-center text-sm text-zinc-500">
+      {{ date }}
+    </p>
+  </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header class="flex items-center justify-center px-6 pt-6 sm:px-10 sm:pt-16">
+    <nav class="flex w-full max-w-lg items-center justify-between gap-3">
+      <NuxtLink to="/" class="flex items-center gap-6 text-xl"> Lucas Ali </NuxtLink>
+
+      <div class="flex gap-3">
+        <NuxtLink to="/blog" class="h-10 focus-visible:outline-none">
+          <Button aria-label="Blog"> Blog </Button>
+        </NuxtLink>
+
+        <Button> Contact </Button>
+
+        <Button size="icon">
+          <Icon name="radix-icons:moon" class="size-5" />
+        </Button>
+      </div>
+    </nav>
+  </header>
+</template>
